9ae33684f6b07cceb538edd9aa3f6dc7ed1f4b2d
chitrachokkalingam <chitra.lingam05@gmail.com>
2024-07-31 21:56:54 +0530

    push employee

---
+++ src/employee/Employee1.java
@@ -0,0 +1,17 @@
+package employee;
+
+public class Employee1 {
+	
+	private void SampleName()
+	{
+		System.out.println("ajith");
+	}
+		
+		public static void main(String[] args)
+		{
+		Employee1 sample = new 	Employee1();
+		sample.SampleName();
+		}
+	}
+
+
